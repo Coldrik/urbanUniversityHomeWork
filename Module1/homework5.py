e9b4000d621d98b7bb1a0c4232eb992808218137
Coldrik <coldrik@yandex.ru>
2024-05-14 22:14:31 +0500

    Изменил в функциях print подход к выводу текста. Заменил "+" и преобразование в str на ","

--- Module1/homework5.py
+++ Module1/homework5.py
@@ -1,11 +1,11 @@
 my_list = ['яблоко', 'груша', 'мандарин', 'манго', 'арбуз']
-print('List: ' + str(my_list))
-print('First element: ' + my_list[0] + '\nLast element: ' + my_list[-1])
-print('Sublist: ' + str(my_list[2: 5]))
+print('List: ', my_list)
+print('First element: ', my_list[0], '\nLast element: ', my_list[-1])
+print('Sublist: ', my_list[2: 5])
 my_list[2] = 'киви'
-print('Modified list: ' + str(my_list))
+print('Modified list: ', my_list )
 my_dict = {'собака': 'dog', 'кошка': 'kat', 'орел': 'eagle'}
-print('\nDictionary: ' + str(my_dict))
-print('Translation: ' + my_dict['собака'])
+print('\nDictionary: ', my_dict)
+print('Translation: ', my_dict['собака'])
 my_dict['волк'] = 'wolf'
-print('Modified dictionary: ' + str(my_dict))
+print('Modified dictionary: ', my_dict)
