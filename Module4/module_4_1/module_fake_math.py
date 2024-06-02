45315c6836b0b4cfc9ebd2c12be282bdaacb56d2
Coldrik <coldrik@yandex.ru>
2024-06-02 21:58:30 +0500

    add Module_4_1.py with module_fake_math.py and module_true_math.py

---
+++ Module4/module_4_1/module_fake_math.py
@@ -0,0 +1,5 @@
+def divide(first_param, second_param):
+    if second_param == 0:
+        return 'Ошибка'
+    else:
+        return first_param / second_param
---
+++ Module4/module_4_1/module_true_math.py
@@ -0,0 +1,7 @@
+from math import inf
+
+def divide(first_param, second_param):
+    if second_param == 0:
+        return inf
+    else:
+        return first_param / second_param
