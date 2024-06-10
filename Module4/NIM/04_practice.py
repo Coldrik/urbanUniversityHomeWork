0826b81a4b1c3b46f2f105b043b6872b2ddf6b44
Coldrik <coldrik@yandex.ru>
2024-06-10 22:44:58 +0500

    add Module_4_practice.py

--- Module4/NIM/04_practice.py
+++ Module4/NIM/04_practice.py
@@ -1,17 +1,23 @@
 from nim_engine import get_bunches, put_stones, is_gameover, take_from_bunch
-from termcolor import cprint
+from termcolor import cprint, colored
+import colorama
+colorama.init()
 
 put_stones()
 user_number = 1
 while True:
+    print(colored('hahah', "blue"))
     cprint('Текущая позиция', color="green")
     cprint(get_bunches(), color="green")
     user_color = 'blue' if user_number == 1 else "yellow"
-    print('Ход игрока {}'.format(user_number))
-    pos = input('Откуда берем?')
-    qua = input('Сколько берем?')
-    take_from_bunch(position=int(pos), quantity=int(qua))
-    if is_gameover():
+    cprint('Ход игрока {}'.format(user_number), color=user_color)
+    pos = input(colored('Откуда берем?', color=user_color))
+    qua = input(colored('Сколько берем?', color=user_color))
+    step_successed = take_from_bunch(position=int(pos), quantity=int(qua))
+    if step_successed:
+        user_number = 2 if user_number == 1 else 1
+    else:
+        cprint('невозможный ход! ', color='red')
         break
     user_number = 2 if user_number == 1 else 1
 
--- Module4/NIM/nim_engine.py
+++ Module4/NIM/nim_engine.py
@@ -2,19 +2,31 @@ from random import randint
 
 
 MAX_BUNCHES = 5
-_holder = []
+MAX_BUNCHES_SIZE = 20
+
+_holder = {}
+
+
+
 def put_stones():
     global _holder
-    _holder = []
-    for i in range(5):
-        _holder.append(randint(1, 20))
+    _holder = {}
+    for i in range(1, MAX_BUNCHES+1):
+        _holder[i] = randint(i, MAX_BUNCHES_SIZE)
 
 
 def take_from_bunch(position, quantity):
-    if 1 <= position <= len(_holder):
-        _holder[position - 1] -= quantity
+    if position in _holder:
+        _holder[position] -= quantity
+        return True
+    else:
+        return False
+
 def get_bunches():
-    return _holder
+    res = []
+    for key in sorted(_holder.keys()):
+        res.append(_holder[key])
+    return res
 
 def is_gameover():
     return sum(_holder) == 0
