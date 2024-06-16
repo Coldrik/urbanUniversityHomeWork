0dae208286cb39bb7e62d965a9783fe4d08edb72
Coldrik <coldrik@yandex.ru>
2024-06-16 18:35:50 +0500

    add module_5_4.py

---
+++ Module5/module_5_4.py
@@ -0,0 +1,17 @@
+class Building():
+
+    building_total = 0
+    def __init__(self, numberOfFloors, nameOfBuilding):
+        self.numberOfFloors = numberOfFloors
+        self.nameOfBuilding = nameOfBuilding
+        Building.building_total += 1
+
+
+listOfBuilding = []
+for i in range(40):
+    a = 'a' + str(i+1)
+    listOfBuilding.append(Building(5, a))
+    print('Создано здание: ', listOfBuilding[i].nameOfBuilding)
+print('Колличсество зданий: ', Building.building_total)
+
+
