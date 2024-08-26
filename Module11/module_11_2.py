3026dd073d9e58fe34ef97c14002b3599d82a5b0
Coldrik <coldrik@yandex.ru>
2024-08-26 22:59:20 +0500

    fix attributes view module_11_2.py

--- Module11/module_11_2.py
+++ Module11/module_11_2.py
@@ -8,15 +8,19 @@ class H:
         print('hello')
 
 def introspection_info(obj):
-    x1 = vars(obj) if '__dict__' in dir(obj) else None
+    x1 = dir(obj)
+    x1_new = []
+    for i in x1:
+        if '__' not in i:
+            x1_new.append(i)
     x2 = dir(obj)
     x3 = inspect.getmodule(obj)
     return {'type': type(obj).__name__,
-            'attributes': x1,
+            'attributes': x1_new,
             'methods': x2,
             'module': x3
             }
 
 h = H()
-someInfo = introspection_info (h)
+someInfo = introspection_info (42)
 print(someInfo)
