aeae426ead8887f57c4244aef4504879afd657dd
Coldrik <coldrik@yandex.ru>
2024-09-23 22:39:20 +0500

    update module_14_4.py and crud_function.py, add module_14_5.py

--- Module14/crud_function.py
+++ Module14/crud_function.py
@@ -1,12 +1,13 @@
 import sqlite3
 
+
 # connection = sqlite3.connect('./product.db')
 # cursor = connection.cursor()
-def initiate_db ():
+def initiate_db_product():
     connection = sqlite3.connect('./product.db')
     cursor = connection.cursor()
     cursor.execute('''
-    CREATE TABLE IF NOT EXISTS Users (
+    CREATE TABLE IF NOT EXISTS Products (
     id INTEGER PRIMARY KEY,
     title TEXT,
     description TEXT NOT NULL,
@@ -16,19 +17,63 @@ def initiate_db ():
     connection.commit()
     connection.close()
 
+
+def initiate_db_user():
+    connection = sqlite3.connect('./product.db')
+    cursor = connection.cursor()
+    cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users (
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER NOT NULL,
+balance INTEGER NOT NULL
+)
+''')
+    connection.commit()
+    connection.close()
+
+
+def add_user(username, email, age):
+    connection = sqlite3.connect('./product.db')
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
+                   (f'{username}', f'{email}', f'{age}', 1000))
+    connection.commit()
+    connection.close()
+
+
+def is_included(username) -> bool:
+    """
+    Вернет True если пользователь есть в таблице
+    """
+    connection = sqlite3.connect('./product.db')
+    cursor = connection.cursor()
+    select_users = cursor.execute('SELECT username FROM Users').fetchall()
+    check = False
+    for select in select_users:
+        check = username == select[0]
+        if check:
+            break
+
+    return check
+
+
+
 def add_product():
     connection = sqlite3.connect('./product.db')
     cursor = connection.cursor()
     for i in range(1, 5):
-        cursor.execute('INSERT INTO Users (title, description, price) VALUES (?, ?, ?)',
+        cursor.execute('INSERT INTO Products (title, description, price) VALUES (?, ?, ?)',
                        (f'Продукт {i}', f'Описание {i}', i * 100))
     connection.commit()
     connection.close()
 
+
 def get_all_products():
     connection = sqlite3.connect('./product.db')
     cursor = connection.cursor()
-    cursor.execute('SELECT * FROM Users')
+    cursor.execute('SELECT * FROM Products')
     users = cursor.fetchall()
     # for user in users:
     #     print(f'Имя: {user[1]} | Почта: {user[2]} | Возвраст: {user[3]} | Баланс: {user[4]}')
--- Module14/module_14_5.py
+++ Module14/module_14_5.py
@@ -10,30 +10,35 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import asyncio
 
 import API
-# initiate_db_product () #  Тестовое заполнение таблицы
-# add_product() #  Тестовое заполнение таблицы
-products = get_all_products()
 
+#initiate_db_product () #  Тестовое заполнение таблицы
+# add_product() #  Тестовое заполнение таблицы
+
+initiate_db_user() #  Тестовое заполнение таблицы
+
+products = get_all_products()
 
 api = API.API
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 kb = ReplyKeyboardMarkup()
-kb.resize_keyboard=True
+kb.resize_keyboard = True
 button = InlineKeyboardButton(text='Расчитать норму калорий', callback_data='calories')
 button2 = InlineKeyboardButton(text='Формулы расчета', callback_data='formulas')
 button3 = InlineKeyboardButton(text='Покупаем', callback_data='buy')
+button4 = InlineKeyboardButton(text='Регистрация', callback_data='reg')
 kb.insert(button)
 kb.insert(button2)
 kb.row(button3)
+kb.insert(button4)
 
 buy_menu = InlineKeyboardMarkup(
     inline_keyboard=[
         [InlineKeyboardButton(text='Product1', callback_data='product_buying'),
-        InlineKeyboardButton(text='Product2', callback_data='product_buying'),
-        InlineKeyboardButton(text='Product3', callback_data='product_buying'),
-        InlineKeyboardButton(text='Product4', callback_data='product_buying')]
+         InlineKeyboardButton(text='Product2', callback_data='product_buying'),
+         InlineKeyboardButton(text='Product3', callback_data='product_buying'),
+         InlineKeyboardButton(text='Product4', callback_data='product_buying')]
     ]
 )
 
@@ -44,30 +49,41 @@ class UserState(StatesGroup):
     weight = State()
 
 
+class RegistrationState(StatesGroup):
+    username = State()
+    email = State()
+    age = State()
+    balance = 1000
+
+
 @dp.message_handler(text='Формулы расчета')
 async def formula(message):
     await message.answer('Формула для расчета калорий:')
     await message.answer('10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5;')
+
 
 @dp.message_handler(text='Расчитать норму калорий')
 async def set_age(message):
     await message.answer('Введите свой возраст:')
     await UserState.age.set()
 
+
 @dp.message_handler(text='Покупаем')
 async def get_buying_list(message):
     i = 1
     for product in products:
         with open(f'pic/pic{i}.jpg', 'rb') as img:
-            await message.answer_photo(img, f'Название: {product [1]} | Описание: {product [2]} | Цена: {product [3]}')
+            await message.answer_photo(img, f'Название: {product[1]} | Описание: {product[2]} | Цена: {product[3]}')
         i += 1
     await message.answer('Выберете продукт для покупки:',
                          reply_markup=buy_menu)
+
 
 @dp.callback_query_handler(text='product_buying')
 async def send_confirm_message(call):
     await call.message.answer('Вы успешно приобрели продукт!')
     await call.answer()
+
 
 @dp.message_handler(state=UserState.age)
 async def set_growth(message, state):
@@ -101,6 +117,36 @@ async def start(message):
                          reply_markup=kb)
 
 
+@dp.message_handler(text='Регистрация')
+async def sing_up(message):
+    await message.answer('Введите имя пользователя (только латинский алфавит):')
+    await RegistrationState.username.set()
+
+
+@dp.message_handler(state=RegistrationState.username)
+async def set_username(message, state):
+    if is_included(message.text):
+        await message.answer('Пользователь существует, введите другое имя (только латинский алфавит):')
+        await RegistrationState.username.set()
+    else:
+        await state.update_data(username=message.text)
+        await message.answer('Введите свой email:')
+        await RegistrationState.email.set()
+
+
+@dp.message_handler(state=RegistrationState.email)
+async def set_email(message, state):
+    await state.update_data(email=message.text)
+    await message.answer('Введите свой возраст:')
+    await RegistrationState.age.set()
+
+@dp.message_handler(state=RegistrationState.age)
+async def set_age(message, state):
+    await state.update_data(age=message.text)
+    data = await state.get_data()
+    print(data)
+    add_user(data['username'], data['email'], data['age'])
+    await state.finish()
 
 
 @dp.message_handler()
