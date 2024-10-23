89064b1b96bc7163f5fce9bd430d49b4f6fb592b
Coldrik <coldrik@yandex.ru>
2024-10-23 23:51:48 +0500

    update module_17_1: add db.py, models.__init__.py, models.task.py, models.user.py

---
+++ Module17/module_17_1/models/user.py
@@ -0,0 +1,21 @@
+from Module17.module_17_1.backend.db import Base
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+from Module17.module_17_1.models import *
+
+class User(Base):
+    __tablename__ = "users"
+    __table_args__ = {'keep_existing': True}
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True, index=True)
+
+    task = relationship("Task", back_populates="users")
+
+from sqlalchemy.schema import CreateTable
+print(CreateTable(User.__table__))
+
