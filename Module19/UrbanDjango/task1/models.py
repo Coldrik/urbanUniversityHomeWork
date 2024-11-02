30a3b5213c410b96a9c7bcdb5070ef0b0cb608c5
Coldrik <coldrik@yandex.ru>
2024-11-02 22:20:48 +0500

    add Module19/Task1

---
+++ Module19/UrbanDjango/task1/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=200)  # имя покупателя(username аккаунта)
+    balance = models.DecimalField(max_digits=6, decimal_places=2)  # баланс(DecimalField)
+    age = models.IntegerField  # возраст
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=200)  # название игры
+    cost = models.DecimalField(max_digits=6, decimal_places=2)  # цена(DecimalField)
+    size = models.DecimalField(max_digits=6, decimal_places=2)  # размер файлов игры(DecimalField)
+    description = models.TextField()  # описание(неограниченное кол-во текста)
+    age_limited = models.BooleanField()  # ограничение возраста 18+ (BooleanField, по умолчанию False)
+
+    # покупатель обладающий игрой (ManyToManyField). У каждого покупателя может быть игра и у каждой
+    # игры может быть несколько обладателей.
+    buyer = models.ManyToManyField(Buyer, related_name='Games')
+
+    def __str__(self):
+        return self.title
